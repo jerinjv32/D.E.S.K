e474c29a19b56e1f29e9f0ff9fd8b66a00925d60
jerinjv32 <jerinjvarghese32@gmail.com>
2025-02-22 22:21:06 +0530

    minor changes

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,12 +1,27 @@
 <?php
     session_start();
-
     if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
         header("Location: login.php");
         exit();
     }
-    session_write_close();
     include('sidebar.php');
+    if(isset($_GET['course_management'])){
+        header("Location:course_management.php");
+        exit();
+    }
+    elseif(isset($_GET['staff_management'])){
+        header("Location:faculty_management.php");
+        exit();
+    }
+    elseif(isset($_GET['student_management'])){
+        header("Location:student_management.php");
+        exit();
+    }
+    elseif(isset($_GET['attendance'])){
+        header("Location:attendance.php");
+        exit();
+    }
+    session_write_close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,42 +37,60 @@
         <h3 class="nav_content1">Dashboard</h3>
     </div>
     <main>
-        <form>
+        <form action="admin_dashboard.php" method="GET">
             <div class="panels">
                 <div class="panel_col1"  >
-                    <input type="image" src="http://localhost:5500/icons/coure_management.png" class="icons" alt="course_management">
+                    <button name="course_management">  
+                        <img src="http://localhost:5500/icons/coure_management.png" class="icons" alt="course_management">
+                    </button>
                     <figcaption class="panel_title" id="title1">Course Management</figcaption>
                 </div>
                 <div class="panel_col2" >
-                    <input type="image" src="http://localhost:5500/icons/staff_management.png" class="icons" alt="staff_management">
+                    <button name="staff_management">
+                        <img src="http://localhost:5500/icons/staff_management.png" class="icons" alt="staff_management">
+                    </button>
                     <figcaption class="panel_title" id="title2">Staff Management</figcaption>
                 </div>
                 <div class="panel_col3" >
-                    <input type="image" src="http://localhost:5500/icons/student_management.png" class="icons" alt="student_management">
+                    <button name="student_management">
+                        <img src="http://localhost:5500/icons/student_management.png" class="icons" alt="student_management">
+                    </button>
                     <figcaption class="panel_title" id="title3">Student Management</figcaption>
                 </div>
                 <div class="panel_col4">
-                    <input type="image" src="http://localhost:5500/icons/attendance.png" class="icons" alt="attendance">
+                    <button name="attendance">
+                        <img src="http://localhost:5500/icons/attendance.png" class="icons" alt="attendance">
+                    </button>
                     <figcaption class="panel_title" id="title4">Attendance</figcaption>
                 </div>
                 <div class="panel_col5" >
-                    <input type="image" src="http://localhost:5500/icons/data_extractor.png" class="icons" name="data_extractor">
+                    <button name="data_extractor">
+                        <img src="http://localhost:5500/icons/data_extractor.png" class="icons" name="data_extractor">
+                    </button>
                     <figcaption class="panel_title" id="title5">Data Extractor</figcaption>
                 </div>
                 <div class="panel_col6" >
-                    <input type="image" src="http://localhost:5500/icons/results.png" class="icons" alt="results">
+                    <button name="results">
+                        <img src="http://localhost:5500/icons/results.png" class="icons" alt="results">
+                    </button>
                     <figcaption class="panel_title" id="title6">Results</figcaption>
                 </div>
                 <div class="panel_col7" >
-                    <input type="image" src="http://localhost:5500/icons/academic.png" class="icons" alt="academic_assessment">
+                    <button name="academic_assessment">
+                        <img src="http://localhost:5500/icons/academic.png" class="icons" alt="academic_assessment">
+                    </button>
                     <figcaption class="panel_title" id="title7">Academic Assessment</figcaption>
                 </div>
                 <div class="panel_col8" >
-                    <input type="image" src="http://localhost:5500/icons/remedial_class.png" class="icons" alt="remedial_class">
+                    <button name="remedial_class">
+                        <img src="http://localhost:5500/icons/remedial_class.png" class="icons" alt="remedial_class">
+                    </button>
                     <figcaption class="panel_title" id="title8">Remedial Class</figcaption>
                 </div>
-                <div class="panel_col9">
-                    <input type="image" src="http://localhost:5500/icons/event.png" class="icons" alt="events">
+                <div class="panel_col9" name="events">
+                    <button>
+                        <img src="http://localhost:5500/icons/event.png" class="icons" alt="events">
+                    </button>
                     <figcaption class="panel_title" id="title9">Events</figcaption>
                 </div>
             </div>
@@ -65,5 +98,3 @@
     </main>
 </body>
 </html>
-
-
