5c97d475e4d3522ce31af9d8f7a2e187d3d97655
jerinjv32 <jerinjvarghese32@gmail.com>
2025-03-03 22:51:35 +0530

    New stuffs

---
+++ add_course.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Management</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>
+        *{
+            font-family: poppins;
+            margin:0px;
+        }
+        nav{
+            height: 53px;
+            width: 100%;
+            background-color: rgb(33, 33, 33);
+        }
+        .nav_content1{
+            display: block;
+            padding-left: 225px;
+            padding-top: 12px;
+            font-size: larger;
+            color: white;
+        }
+        main{
+            height: 100px;
+            border: 1px black solid;  
+            margin: 20px 20px 20px 228px;
+            border-radius: 5px;
+            padding:20px 10px 0 20px;
+            box-shadow: 0 5px 5px rgba(0,0,0,0.25);
+            min-height: 500px;
+            max-height: 100%;
+            overflow: auto;
+        }
+        #back{
+            color: gray;
+            background-color: rgb(33, 33, 33);
+            border: none;
+            font-size: 1.05em;
+            font-weight: bold;
+            transition: color 0.25s linear;
+        }
+        #back:hover{
+            color: white;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <?php include('sidebar.php') ?>
+    <nav>
+        <form>
+            <h3 class="nav_content1"><input type="submit" name="backto" id="back" value="Course Management"> / Add course</h3>
+        </form>
+    </nav>
+    <main></main>
+</body>
+</html>
